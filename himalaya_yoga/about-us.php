91639d9134ce75cd38fe5a4f825fb1adcab5832d
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-07 18:29:05 +0530

    about us page -
    contents and pictures are done.

---
+++ himalaya_yoga/about-us.php
@@ -0,0 +1,28 @@
+<?php include "header.php"; ?>
+
+<!-- Background Image -->
+<img src="assets/images/b-1.jpg" alt="Background Image" 
+     class="img-fluid position-fixed w-100 z-n1">
+
+<!-- Hero Section -->
+<div class="container-fluid min-vh-100 d-flex justify-content-center align-items-center position-relative z-1">
+  <h1 class="text-center text-white display-4">About Us</h1>
+</div>
+
+<!-- About Section -->
+<div class="container py-5">
+  <div class="row flex-lg-row-reverse align-items-center g-5">
+    <div class="col-10 col-sm-8 col-lg-6">
+      <img src="assets/images/photo-1.jpg" alt="About Image" class="d-block mx-auto img-fluid" loading="lazy">
+    </div>
+    <div class="col-lg-6">
+      <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">About us</h1>
+      <p class="lead">
+        Himalaya Yoga and Holistic Living Institute (HYHLI) was constituted in 2015 with a view to pursuing the mission of projecting the authentic teachings of yoga and spiritual living to the world. HYHLI is an autonomous body with no other organization and administrative or legal associations.
+      </p>
+      <button type="button" class="btn btn-warning px-4 rounded-pill">Read more</button>
+    </div>
+  </div>
+</div>
+
+<?php include "footer.php"; ?>
