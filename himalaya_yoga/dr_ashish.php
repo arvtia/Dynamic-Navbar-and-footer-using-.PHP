e42b915e6dc45c15a84aaf5123affb5b6ef59bc0
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-08 14:00:14 +0530

    contents - instructors

---
+++ himalaya_yoga/dr_ashish.php
@@ -0,0 +1,42 @@
+<?php include "header.php" ?>
+
+<div class="container-fluid pt-5">
+     <!-- Image & Text Section -->
+     <div class="row justify-content-center align-items-center py-5">
+        <div class="col-md-5 text-center text-md-center">
+            <figure>
+                <img src="assests/images/dr-ashish.jpeg" class="img-fluid rounded pt-4" alt="Master Shivachittam">
+            </figure>
+        </div>
+        <div class="col-md-5">
+            <h3 class="fs-1 fw-bold text-start">Doctor Ashish</h3>
+            <p class="fs-5">
+                <strong>Dr. Ashish Kumar,</strong> born and brought up in MP. Has mastery over dozens of Alternative Therapies including all the aspects of yoga, meditation and Shatkarma as full body detoxification.
+            </p>
+        </div>
+    </div>
+    <div class="row justify-content-center py-5">
+        <div class="col-lg-8 col-md-10">
+            <p class="fs-5">
+                Specialized in Acupuncture, Moxibation, cupping therapy, detoxification of whole body, liver and gallbladder cleansing, kidney cleansing, Reflexology, Sujok, Color Therapy, Seed therapy, Magnet Therapy, Numerology, Whole body Acupuncture and Ayurvedic way of diagnosis and treatment accordingly as per disease.
+                His Massage and Naturopathy way of treatment gives the heavenly feeling of relaxation to the patients.  
+            </p>
+            <p class="fs-5">
+                His way of treatment in Panchkarma and Shatkarma for complete detoxification is really amazing and out of the box. He is well recognized and certified Acupuncture Doctor from China, Sri Lanka, and Government of Maharashtra.     </p>
+            <p class="fs-5">
+                His last 10 years of Treatment and Teaching experience in the field of Acupuncture, Acupressure and all the above mentioned Alternative Therapies make him special living legendary of this Alternative Therapies field.
+            </p>
+            <p class="fs-5">
+                He believes and practices No medicine policy.
+                According to Dr. Ashish, All diseases have generation in the body so the solution is also in the body. Not outside the body. Therefore His holistic approach to deal with any disease is just Marvelous.
+            </p>
+        </div>
+    </div>
+
+
+
+
+</div>
+
+
+<?php include "footer.php" ?>
