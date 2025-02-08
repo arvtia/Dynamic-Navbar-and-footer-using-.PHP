fc44971aa5c09e5f4b1531fc7d85f86d851eff61
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-08 18:28:19 +0530

    nature therapy content

---
+++ himalaya_yoga/natur_therapy.php
@@ -0,0 +1,38 @@
+<?php include "header.php" ?>
+
+<div class="container-fluid min-vh-100 d-flex justify-content-center align-items-center text-center position-relative" 
+     style="background: url('https://himalayayoga.net/wp-content/uploads/2022/05/benefits-yoga-fb.jpg') no-repeat center center; background-size: cover; background-attachment: fixed;">
+    
+
+     <div class="bg-white bg-opacity-75 p-5 rounded shadow">
+        <h1 class="fw-bold">The Benefits of Yoga</h1>
+        <p class="fs-5">Yoga helps improve flexibility, strength, and mindfulness. It enhances overall well-being and reduces stress.</p>
+    </div>
+
+</div>
+
+<div class="container-fluid text-center bg-white py-5 justify-content-center w-75">
+    <h4 class="text-dark">Nature therapy is defined as “a set of practices aimed at achieving ‘preventive medical effects’ through exposure to natural stimuli that render a state of physiological relaxation and boost the weakened immune functions to prevent diseases</h4>
+</div>
+
+<div class="row bg-white px-5 py-5 mx-auto">
+    <!-- Text Column -->
+    <div class="col-12 col-md-6 d-flex flex-column align-items-start">
+        <p class="mb-0 py-3 fs-5 ">
+            Nature has long been known to have calming effects on the mind. The beauty and serenity of nature create a sense of peace that you usually won’t be able to find in busy urban environments. Thus, spending time in nature is one of the most effective ways to reduce everyday stress and mental fatigue.
+        </p>
+        <p class="mb-0 py-3 fs-5 ">
+        Nature can generate a multitude of positive emotions, such as calmness, joy, creativity and can facilitate concentration. <strong> Nature connectedness is also associated with lower levels of poor mental health; in particular lower depression and anxiety levels.</strong>
+        </p>
+    </div>
+
+    <!-- Image Column -->
+    <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+        <img src="assests/images/photo-box-1.jpg" 
+             alt="Nature Image" 
+             class="img-fluid" 
+             style="max-width: 100%; height: 45vhpx; object-fit: cover;">
+    </div>
+</div>
+
+<?php include "footer.php" ?>
