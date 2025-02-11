cf2ff9bb32d5540e81331a8294730246fde5007c
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-11 17:09:33 +0530

    more pages
    accomodations- need to be made changes 
    blog - its empty for now
    courses - added contents and forms
    events- empty 
    pratyahara..- empty
    search_result- a redirect page for missing contents.

---
+++ himalaya_yoga/search_result.php
@@ -0,0 +1,24 @@
+<?php include "header.php" ?>
+
+<div class="row mt-5  py-5">
+        <div class="col-md-8 col-sm-4 mx-auto pt-5 lh-lg px-4 ">
+            <div class="small fw-light fs-4">It seems we can’t find what you’re looking for. Perhaps searching can help.</div>
+            <div class="input-group">
+                <input class="form-control border-end-0 border" type="search" value="search" id="content-search-input">
+                <span class="input-group-append">
+                    <button class="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border ms-n5" type="button">
+                        <img src="assests/images/search-icon.svg" alt="search_icon">
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <p id="demo"></inner></p>
+        </div>
+    </div>
+
+
+<?php include "footer.php" ?>
