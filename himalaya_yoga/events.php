4ed6d7d98423fc9e931e55e671e64875695de2c8
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-18 17:18:53 +0530

    Update events.php
    updated the contents

---
+++ himalaya_yoga/events.php
@@ -0,0 +1,43 @@
+<?php include "header.php" ?>
+
+
+<!-- Background Image -->
+<img src="assests/images/health-m.jpg" alt="Background Image" 
+     class="img-fluid position-fixed w-100 z-n1 vh-100 object-fit-cover">
+
+<div class="text-center pt-5 bg-white">
+
+</div>
+
+<div class="container-fluid mx-auto py-3 bg-white">
+    <div class="text-dark text-start pt-5 bg-white my-auto">
+        <h1 class="py-2 px-5 fw-bolder bg-white">Latest Events</h1>
+    </div>
+</div>
+
+<div class="row-fuild d-flex">
+    <div class="col-lg-2 col-md-1 bg-white">
+
+    </div>
+
+    <div class="col-lg-8 col-md-10 " >
+        <div class=" text-center align-items-center my-auto d-flex align-items-center justify-content-center min-vh-100">
+            <a href="#" style="text-decoration: none;"><h1 class="align-items-center text-warning text-center">Uniting Nation from 2nd to 5th June</h1></a>
+        </div>
+    </div>
+
+    <div class="col-lg-2 col-md-1 bg-white"> 
+
+    </div>
+</div>
+<div class="text-center py-5 bg-white">
+
+</div>
+
+
+
+
+
+
+
+<?php include "footer.php" ?>
