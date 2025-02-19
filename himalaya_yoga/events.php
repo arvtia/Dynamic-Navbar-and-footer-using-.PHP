56de42b4d8b85c97f187421b78775cbf955bde04
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-19 15:29:52 +0530

    Update events.php
    added carousel , video , images , and events with buttons- links

--- himalaya_yoga/events.php
+++ himalaya_yoga/events.php
@@ -36,6 +36,135 @@
 
 
 
+<!--bootstrap carousel applications-->
+
+<!-- Bootstrap 5 Carousel -->
+
+<div class="text-center bg-white py-4">
+    <h1 class="bg-white text-warning text-center">Outdoor Events -"Feel the nature around you"</h1>
+</div>
+<div class="container-fuild">
+    <div class="row py-5 bg-white px-3">
+        <div class="col-lg-3 col-md-4 col-sm-6 py-3 px-2">
+            <img src="assests/images/yogapeeers.avif" alt="" class="img-fluid object-fit-cover w-100">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 py-3 px-2">
+            <img src="assests/images/Yogapeeps.jpg" alt="" class="img-fluid object-fit-cover">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 py-3 px-2">
+            <img src="assests/images/Yogapeeps3.jpg" alt="" class="img-fluid object-fit-cover w-100">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 py-3 px-2">
+            <img src="assests/images/yogapeers2.avif" alt="" class="img-fluid object-fit-cover">
+        </div>
+    </div>
+</div>
+
+
+
+<div id="cardCarousel" class="carousel slide bg-warning" data-bs-ride="carousel" id="carousel-ki-to">
+    <!-- Carousel Controls (Adjusted Height) -->
+    <button class="carousel-control-prev custom-control" type="button" data-bs-target="#cardCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next custom-control" type="button" data-bs-target="#cardCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+
+    <!-- Carousel Inner -->
+    <div class="carousel-inner">
+        <div class="carousel-item active" id="item-carousel">
+            <div class="d-flex justify-content-center pt-5">
+                <div class="card mx-2" style="width: 18rem; height: 550px;">
+                    <img src="assests/images/yoga-car1.jpg" class="card-img-top img-fluid" alt="Card Image 1">
+                    <div class="card-body position-absolute top-50 text-dark bg-white w-100">
+                        <h5 class="card-title">Full Moon Meditation & Purification Ceremony</h5>
+                        <p class="card-text"><strong>Date & Time:</strong> February 12, 2025, 6:30 PM. <br> <strong>Location: </strong> Ananda Sangha Delhi, N-100, Panchsheel Park, New Delhi</p>
+                        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Read more</a>
+                    </div>
+                </div>
+                <div class="card mx-2 d-none d-md-block" style="width: 18rem; height: 550px;">
+                    <img src="assests/images/Yogapeeps1.jpg" class="card-img-top img-fluid object-fit-cover" alt="Card Image 2">
+                    <div class="card-body position-absolute top-50 text-dark bg-white w-100">
+                        <h5 class="card-title">Introduction to Meditation and the Path of Kriya Yoga</h5>
+                        <p class="card-text"><strong>Date & Time:</strong> February 14, 2025, 7:00 PM. <br> <strong>Location:</strong> Ananda Sangha Delhi, N-100, Panchsheel Park, New Delhi</p>
+                        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Read more</a>
+                    </div>
+                </div>
+                <div class="card mx-2 d-none d-lg-block" style="width: 18rem; height: 550px;">
+                    <img src="assests/images/yogapeers2.avif" class="card-img-top" alt="Card Image 3">
+                    <div class="card-body card-body position-absolute top-50 text-dark bg-white w-100">
+                        <h5 class="card-title">Introduction to Paramhansa Yogananda’s Nine-Day Cleansing and Vitalizing Diet</h5>
+                        <p class="card-text"><strong>Date & Time:</strong> February 15, 2025, 5:45 PM. <strong>Location:</strong>  Online</p>
+                        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class="d-flex justify-content-center pt-5">
+                <div class="card mx-2 rounded" style="width: 18rem; height: 550px;">
+                    <img src="assests/images/Yogapeeps3.jpg" class="card-img-top" alt="Card Image 4">
+                    <div class="card-body card-body position-absolute top-50 text-dark bg-white w-100">
+                        <h5 class="card-title">Kriya Preparation 1 - Part 2</h5>
+                        <p class="card-text"><strong>Date & Time:</strong> March 30, 2025, 11:30 AM. <br><strong>Location:</strong> Ananda Sangha Delhi, N-100, Panchsheel Park, New Delhi</p>
+                        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Read more</a>
+                    </div>
+                </div>
+                <div class="card mx-2 d-none d-md-block rounded" style="width: 18rem; height: 550px;">
+                    <img src="assests/images/yoga-car6.webp" class="card-img-top" alt="Card Image 5">
+                    <div class="card-body card-body position-absolute top-50 text-dark bg-white w-100">
+                        <h5 class="card-title">World Meditation Day Special Programme</h5>
+                        <p class="card-text"><strong>Date & Time:</strong> December 21, 2025, 10:00 AM. <strong>Location:</strong> YSS, Hyderabad</p>
+                        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Read more</a>
+                    </div>
+                </div>
+                <div class="card mx-2 d-none d-lg-block rounded" style="width: 18rem; height: 550px;">
+                    <img src="assests/images/yoga-car4.jpg" class="card-img-top" alt="Card Image 6">
+                    <div class="card-body card-body position-absolute top-50 text-dark bg-white w-100">
+                        <h5 class="card-title">Guru Purnima Celebration</h5>
+                        <p class="card-text"><strong>Date & Time:</strong> July 6, 2025, 9:00 AM. <br> <strong>Location:</strong>  Isha Yoga Center, Coimbatore</p>
+                        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-white">
+    <div class="text-center fs-1 pt-4 fw-bolder">
+        Reborn with New Energy ( Spiritually)
+    </div>
+    <div class="row mx-auto d-flex py-5">
+        <div class="col-lg-2 col-md-4">
+
+        </div>
+        <div class="col-lg-8 col-md-4">
+            <video src="assests/images/video04.mp4" autoplay muted loop class="w-100 vh-100 shadow object-fit-cover"></video>
+        </div>
+        <div class="col-lg-2 col-md-4">
+
+        </div>
+    </div>
+
+</div>
+
+<div class="text-center bg-white py-5">
+    <a href="courses.php" class="btn btn-outline-light px-4 py-2 rounded-0">Register now</a>
+</div>
+
+
+<div class="container-fluid">
+    <div class="row"></div>
+</div>
+
+
+
+
+
+
 
 
 
