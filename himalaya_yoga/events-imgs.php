91574712cb3c9ccab8674e15c22c5a8b30bde28c
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-24 17:45:06 +0530

    events-page ( images)

---
+++ himalaya_yoga/events-imgs.php
@@ -0,0 +1,156 @@
+<?php include ("header.php") ?>
+
+
+<div class="container-fluid pt-5">
+    <div class="col-8 pt-4 mx-auto">
+        <div class="row px-2 pt-5 pb-3">
+            <div class="col-3" data-aos="fade-up" data-aos-duration="2500">
+                <div class="position-relative">
+                    <img src="assests/images/event-collage10.png" alt=""  class="img-fluid object-fit-cover" style="height:400px;">
+                        <div class="text-center position-absolute top-50 start-0 z-1 bg-black">
+                            <p class="text-white">
+                                <strong class="text-warning">Lotus Flower:</strong>"At himalaya Yoga, we believe in the purity and enlightenment symbolized by the lotus flower. This beautiful pink lotus, with its delicate petals and vibrant yellow center.
+                            </p>
+                        </div>
+                </div>
+            </div>
+            <div class="col-3" data-aos="fade-up" data-aos-duration="2000">
+                <div class="position-relative">
+                    <img src="assests/images/event-collage2.png" alt=""  class="img-fluid object-fit-cover" style="height:400px;">
+                        <div class="text-center position-absolute top-50 start-0 z-1 bg-black">
+                            <p class="text-white">
+                                <strong class="text-warning">Yoga Trainer with Students:</strong> "Join us at Himalaya Yoga, where our skilled yoga trainer leads a group of dedicated students in a serene, sunlit studio. Experience the perfection. 
+                            </p>
+                        </div>
+                </div>
+            </div>
+            <div class="col-3" data-aos="fade-up" data-aos-duration="1500">
+                <div class="position-relative">
+                    <img src="assests/images/event-collage3.png" alt=""  class="img-fluid object-fit-cover" style="height:400px;">
+                        <div class="text-center position-absolute top-50 start-0 z-1 bg-black">
+                            <p class="text-white">
+                                <strong class="text-warning">Lady Practicing Yoga Alone on a Yoga Mat:</strong> "At himalaya Yoga, we offer a peaceful environment for personal practice. This image captures a dedicated woman practicing yoga.
+                        </div>
+                </div>
+            </div>
+            <div class="col-3" data-aos="fade-up" data-aos-duration="1000">
+                <div class="position-relative">
+                    <img src="assests/images/event-collage4.png" alt=""  class="img-fluid object-fit-cover" style="height:400px;">
+                        <div class="text-center position-absolute top-50 start-0 z-1 bg-black">
+                            <p class="text-white">
+                                <strong class="text-warning">Woman Practicing Mindfulness:</strong> "Discover the essence of mindfulness at Himalaya Yoga. This serene woman, sitting cross-legged in a traditional meditation pose, reflects the inner calm.
+                        </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="text-center">
+        <h2>Never Seen Before Experience</h2>
+    </div>
+    <div class="col-6 mx-auto pt-3 pb-4">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="assests/images/event-collage5.png" alt="e-5" class="img-fluid object-fit-cover w-100" style="height:500px">
+            </div>
+            <div class="col-lg-6 d-flex flex-column">
+                <div class="text-dark">
+                    <img src="assests/images/event-collage9.png" alt="e-2" style="width:100%;">
+                </div>
+                <div class="text-start">
+                    <h1>Journey to Inner Peace</h1>
+                    <br>
+                    <p>"Diving into on a transformative journey to inner peace with our comprehensive yoga practices and meditation techniques."</p>
+                </div>
+                <!-- <img src="assests/images/event-collage6.png" alt="e-6" class="img-fluid object-fit-cover" style="height:320px"> -->
+            </div>
+        </div>
+        <div class="row pt-4">
+            <div class="col-6">
+            <div class="text-dark">
+                    <img src="assests/images/event-collage7.png" alt="e-2" style="width:100%;">
+                </div>
+                <div class="text-start">
+                    <h1>The Art of Breath</h1>
+                    <br>
+                    <p>"Discover the art of breath control with our Pranayama classes, designed to enhance your physical and mental well-being."</p>
+                </div>
+                <!-- <img src="assests/images/event-collage7.png" alt="e-7" class="img-fluid object-fit-cover w-100" style="height:320px"> -->
+            </div>
+            <div class="col-6">
+                <img src="assests/images/event-collage12png.avif" alt="e-9" class="img-fluid object-fit-cover w-100" style="height:500px">
+            </div>
+        </div>
+        <!-- hidden div.. will appear on onclick function -->
+        <div class="text-center" id="block-c-1">
+            <div class="row pt-4">
+                <div class="col-6">
+                    <img src="assests/images/event-collage7.png" alt="e-7" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+                <div class="col-6">
+                    <img src="assests/images/event-collage11.png" alt="e-9" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+            </div>
+            <div class="row pt-4">
+                <div class="col-6">
+                    <img src="assests/images/event-collage7.png" alt="e-7" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+                <div class="col-6">
+                    <img src="assests/images/event-collage11.png" alt="e-9" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+            </div>
+        </div>
+        <!-- block of text with images to be appeard on second click read more -->
+        <div class="text-center" id="block-c-2">
+            <div class="row pt-4">
+                <div class="col-6">
+                    <img src="assests/images/event-collage7.png" alt="e-7" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+                <div class="col-6">
+                    <img src="assests/images/event-collage11.png" alt="e-9" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+            </div>
+            <div class="row pt-4">
+                <div class="col-6">
+                    <img src="assests/images/event-collage7.png" alt="e-7" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+                <div class="col-6">
+                    <img src="assests/images/event-collage11.png" alt="e-9" class="img-fluid object-fit-cover w-100" style="height:320px">
+                </div>
+            </div>
+        </div>
+       
+
+
+
+
+
+        <dov class="row py-3">
+            <div class="col-4" >
+
+            </div>
+            <div class="col-4 justify-content-center d-flex" >
+                <button type="button" class="btn btn-outline-white rounded-0 ">See more !</button>
+            </div>
+            <div class="col-">
+
+            </div>
+        </dov>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include ("footer.php") ?>
