43241becbc162c30a9b2e7f1c09914c423d634cd
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-26 17:36:00 +0530

    Update eventsVideo.php
    video contents arranged as per the requirement.

--- himalaya_yoga/eventsVideo.php
+++ himalaya_yoga/eventsVideo.php
@@ -13,39 +13,43 @@
 </div>
 
 <div class="container-fluid pt-2">
-    <div class="col-lg-10 col-md-10 col-sm-10 mx-auto pt-2">
+    <div class="col-lg-10 col-md-12 col-sm-12 mx-auto pt-2">
         <div class="row pt-5">
-            <div class="col-lg-8 col-md-8 col-sm-10 " id="videoMain">
-                <video src="assests/images/vide01.mp4" class="w-100 vh-100 object-fit-cover" type="video/mp4" autoplay loop controls id="first-video-link"></video>
-                <div class="text-start py-3">
-                    <h4>At Himlayala Yoga, You will be not only practicing yoga, but also being a part of our Community </h4>
-                    <p>Practicing yoga offers numerous physical, mental, and emotional benefits that cater to individuals of all ages and fitness levels. It promotes flexibility, strength, and balance, which can help prevent injuries and improve overall physical health. Additionally, yoga encourages mindfulness and stress reduction, fostering a sense of inner peace and mental clarity. For anyone interested in diving deeper into yoga, joining Himalaya Yoga is an excellent choice. With its comprehensive collection of yoga videos, you can learn from skilled instructors and follow along at your own pace. The serene environment and expertly designed programs at Himalaya Yoga ensure that you receive the full spectrum of yoga's benefits, making it an ideal destination for anyone looking to enhance their well-being through this ancient practice. 🌿🧘‍♂️</p>
+            <div class="col-lg-8 col-md-12 col-sm-10 mx-auto" id="videoMain">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <video src="assests/images/vide01.mp4" class="w-100 object-fit-cover" type="video/mp4" autoplay loop controls id="first-video-link"></video>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-10">
-                <div class="col-lg-10 col-md-10 col-sm-8 mx-auto" style=" height: 80vh; overflow-y: scroll;" id="mustbelateral">
-                    <div class="row d-flex flex-row">
-                        <div class="col-lg-12 col-md-10 col-sm-10 mx-auto" id="fs-video-1"> 
-                            <video src="assests/images/video04.mp4" class="video-fluid w-100" controls autoplay loop id="second_vide_link"></video>
-                        </div>
-                        <div class="col-lg-12 col-md-10 col-sm-10 mx-auto" id="fs-video-2">
-                            <video src="assests/images/video001.mov" class="video-fluid w-100" controls loop autoplay></video>
-                        </div>
-                        <div class="col-lg-12 col-md-10 col-sm-10 mx-auto" id="fs-video-3">
-                            <video src="assests/images/video0002.mov" class="video-fluid w-100" controls loop autoplay></video>
-                        </div>
-                        <div class="col-lg-12 col-md-10 col-sm-10 mx-auto" id="fs-video-4">
-                            <video src="https://videos.pexels.com/video-files/11274562/11274562-uhd_2560_1440_24fps.mp4" class="video-fluid w-100" controls loop autoplay></video>
-                        </div>
-                        <div class="col-lg-12 col-md-10 col-sm-10 mx-auto" id="fs-video-4">
-                            <video src="https://videos.pexels.com/video-files/5837789/5837789-uhd_2560_1440_24fps.mp4" class="video-fluid w-100" controls loop autoplay></video>
-                        </div>
-                        <div class="col-lg-12 col-md-10 col-sm-10 mx-auto" id="fs-video-4">
-                            <video src="https://videos.pexels.com/video-files/3327806/3327806-hd_1920_1080_24fps.mp4" class="video-fluid w-100" controls loop autoplay></video>
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <!-- scroll on overflow hight more than 80 vh -->
+                <div class="row mx-auto">
+                    <div class="col-lg-10 col-md-10 col-sm-12 mx-auto" style=" overflow-y: scroll; overflow-x:scroll;" id="Row-col-res">
+                        <div class="row d-flex flex-row">
+                            <div class="col-lg-12 col-md-2 col mx-auto" id="fs-video-1"> 
+                                <video src="assests/images/video04.mp4" class="video-fluid w-100"  autoplay loop id="second_vide_link"></video>
+                            </div>
+                            <div class="col-lg-12 col-md-2 col mx-auto" id="fs-video-2">
+                                <video src="assests/images/video001.mov" class="video-fluid w-100"  loop autoplay></video>
+                            </div>
+                            <div class="col-lg-12 col-md-2 col mx-auto" id="fs-video-3">
+                                <video src="assests/images/video0002.mov" class="video-fluid w-100"  loop autoplay></video>
+                            </div>
+                            <div class="col-lg-12 col-md-2 col mx-auto" id="fs-video-2">
+                                <video src="assests/images/video001.mov" class="video-fluid w-100"  loop autoplay></video>
+                            </div>
+                            <div class="col-lg-12 col-md-2 col mx-auto" id="fs-video-2">
+                                <video src="assests/images/video001.mov" class="video-fluid w-100"  loop autoplay></video>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 py-3 px-3">
+                    <h4>At Himlayala Yoga, You will be not only practicing yoga, but also being a part of our Community </h4>
+                    <p>Practicing yoga offers numerous physical, mental, and emotional benefits that cater to individuals of all ages and fitness levels. It promotes flexibility, strength, and balance, which can help prevent injuries and improve overall physical health. Additionally, yoga encourages mindfulness and stress reduction, fostering a sense of inner peace and mental clarity. For anyone interested in diving deeper into yoga, joining Himalaya Yoga is an excellent choice. With its comprehensive collection of yoga videos, you can learn from skilled instructors and follow along at your own pace. The serene environment and expertly designed programs at Himalaya Yoga ensure that you receive the full spectrum of yoga's benefits, making it an ideal destination for anyone looking to enhance their well-being through this ancient practice. 🌿🧘‍♂️</p>
+                </div>
         </div>
     </div>
 </div>
@@ -60,36 +64,37 @@
 
 
 <div class="container-fluid pt-4 pb-4">
-    <div class="col-lg-12 col-md-10 col-sm-10 mx-auto">
-        <div class="row justify-content-evenly">
-            <div class="col-lg-2 col-md-3 col-sm-6">
-                <div class="embed-responsive embed-responsive-4by3">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/yPK7ISPEu3M?si=gn4XpklWXz9AL8Sm&amp;controls=0" Controls></iframe>
+    <div class="col-lg-12 col-md-10 col-sm-8 mx-auto">
+        <div class="row justify-content-between">
+            <div class="col-lg-2 col-md-5 col-sm-6">
+                <div class="ratio ratio-16x9 custom-iframe">
+                    <iframe src="https://www.youtube.com/embed/yPK7ISPEu3M?si=gn4XpklWXz9AL8Sm&amp;controls=0" class=""></iframe>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-6 ">
-                <div class="embed-responsive embed-responsive-4by3">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/24RYHE3HlCQ?si=8mzbjyIDL5p068wf&amp;controls=0" Controls></iframe>
+            <div class="col-lg-2 col-md-5 col-sm-6">
+                <div class="ratio ratio-16x9 custom-iframe">
+                    <iframe src="https://www.youtube.com/embed/24RYHE3HlCQ?si=8mzbjyIDL5p068wf&amp;controls=0" class=""></iframe>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-6">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZVd54YYdf3o?si=hIuQEGXD_onSWTzT&amp;controls=0" Controls></iframe>
+            <div class="col-lg-2 col-md-5 col-sm-6">
+                <div class="ratio ratio-16x9 custom-iframe">
+                    <iframe src="https://www.youtube.com/embed/ZVd54YYdf3o?si=hIuQEGXD_onSWTzT&amp;controls=0" class=""></iframe>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-6">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/brjAjq4zEIE?si=7GNaFQxOSL2qLF1C&amp;controls=0" Controls></iframe>
+            <div class="col-lg-2 col-md-5 col-sm-6">
+                <div class="ratio ratio-16x9 custom-iframe">
+                    <iframe src="https://www.youtube.com/embed/brjAjq4zEIE?si=7GNaFQxOSL2qLF1C&amp;controls=0" class=""></iframe>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-6 ">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/yPK7ISPEu3M?si=gn4XpklWXz9AL8Sm&amp;controls=0" Controls></iframe>
+            <div class="col-lg-2 col-md-5 col-sm-6">
+                <div class="ratio ratio-16x9 ">
+                    <iframe src="https://www.youtube.com/embed/yPK7ISPEu3M?si=gn4XpklWXz9AL8Sm&amp;controls=0" class=""></iframe>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 
 
 
